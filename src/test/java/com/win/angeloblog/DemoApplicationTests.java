9b3a4ba52313c14748955f911f9577ad38816508
AJ Grant <angelojgrant@gmail.com>
2020-06-23 08:16:02 -0400

    first commit

---
+++ src/test/java/com/win/angeloblog/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.win.angeloblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
